02c0a66580df97f42edbff93a79cfef47f046778
eden lane <edencore@gmail.com>
2017-06-11 17:32:34 +0300

    Removed user-list component; functionality moved to User component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component'
 import { AlbumsComponent } from './components/albums/albums.component';
-import { UserListComponent } from './components/users/user-list/user-list.component';
 import { PhotosComponent } from './components/photos/photos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserListComponent,
     AlbumsComponent,
     PhotosComponent
   ],
